4225e6e6c203f2666f9884cbfb62efe9ef17e316
heyli(李成熙) <heyli@tencent.com>
2015-04-10 16:41:31 +0800

    优化代码结构

--- fchart.js
+++ fchart.js
@@ -1,6 +1,53 @@
 /**
  * Created by heyli on 2015/1/26.
  */
+var utils = {
+
+    sort: function(raw, sorted) {
+        var sortTable = [];
+        for (key in raw) {
+            sortTable.push([key, raw[key].figure]);
+        }
+        sortTable.sort(function(a, b) {return b[1] - a[1]});
+
+        // resume other data field value
+        for (key in sortTable) {
+            var index = sortTable[key][0];
+            sorted[key] = {};
+            for (k in raw[index]) {
+                sorted[key][k] = raw[index][k];
+            }
+        }
+    },
+
+    getTotal: function(raw) {
+        var total = 0;
+        for (key in raw) {
+            total += raw[key].figure;
+        }
+        return total;
+    },
+
+    getPercentage: function(sorted, percentage, total) {
+        for (key in sorted) {
+            percentage[key] = sorted[key].figure / total;
+        }
+    },
+
+    getRadius: function(deg) {
+        return deg / 180 * Math.PI;
+    },
+
+};
+
+function fdata(data) {
+    this.raw = data;
+    this.sorted = {};
+    this.info = {};
+    this.total = 0;
+    this.percentage = {};
+}
+
 function fchart(opt) {
     this.canvas = opt.wrapper;                         // canvas
     this.ctx = opt.wrapper.getContext('2d');           // canvas context
@@ -9,95 +56,36 @@ function fchart(opt) {
     this.r = opt.r || 100;                              // piechart radius
     this.type = opt.type || 'piechart';
     this.lineWidth = opt.lineWidth || 50;
-    // this.img = new Image();                             // icon image
+    this.align = opt.align || 'center';
     var self = this;
 
-    this.data = {
-
-        // raw data
-        raw: undefined,
-
-        // sorted data
-        sorted: {},
-
-        // data for drawing
-        info: {},
-
-        // total figure
-        total: 0,
-
-        //percentage object
-        percentage: {
-
-        },
-
-        // initialization
-        init: function(rawData) {
-            this.raw = rawData;
-            this.getTotal();
-            this.sort();
-            this.getPercentage();
-
-            console.log(this.raw);
-            console.log(this.total);
-            console.log(this.sorted);
-            console.log(this.percentage);
-        },
-
-        // sort raw data
-        sort: function() {
-            // sort data based on figure
-            var sortTable = [];
-            for (key in this.raw) {
-                sortTable.push([key, this.raw[key].figure]);
-            }
-            sortTable.sort(function(a, b) {return b[1] - a[1]});
-
-            // resume other data field value
-            for (key in sortTable) {
-                var index = sortTable[key][0];
-                this.sorted[key] = {};
-                for (k in this.raw[index]) {
-                    this.sorted[key][k] = this.raw[index][k];
-                }
-            }
-        },
-
-        getTotal: function() {
-            for (key in this.raw) {
-                this.total += this.raw[key].figure;
-            }
-        },
-
-        getPercentage: function() {
-            for (key in this.sorted) {
-                this.percentage[key] = this.sorted[key].figure / this.total;
-            }
-        },
-    };
-
-    this.data.init(opt.data);
-    this.getWrapperSize();
-    this.init();
-}
-
-// initialization
-fchart.prototype.init = function() {
-    this.index = 0;
-    // this.changedDeg = 10;
+    this.data = new fdata(opt.data);
+    this.data.total = utils.getTotal(this.data.raw);
+    utils.sort(this.data.raw, this.data.sorted);
+    utils.getPercentage(this.data.sorted, this.data.percentage, this.data.total);
     
+    this.getWrapperSize();
     this.draw();
-    // this.drawTotalNumber();
-    // this.drawLabel();
-};
-
+}
 
 // get wrapper size and set canvas size
 fchart.prototype.getWrapperSize = function() {
     this.canvas.width = this.canvas.parentNode.clientWidth * 2;
     this.canvas.height = this.canvas.parentNode.clientHeight * 2;
     this.canvas.style.cssText = '-webkit-transform: translateX(-' + (this.canvas.width / 4) + 'px) scale(0.5);-webkit-transform-origin: 50% 0';
-    this.cx = this.canvas.clientWidth / 2;
+    
+    switch(this.align){
+        case 'left':
+            this.cx = this.r + this.lineWidth;
+            break;
+        case 'right':
+            this.cx = this.canvas.clientWidth - this.r - this.lineWidth;
+            break;
+        default:
+            this.cx = this.canvas.clientWidth / 2;
+            break;
+    }
+
     this.cy = this.canvas.clientHeight / 2;
 };
 
@@ -107,10 +95,17 @@ fchart.prototype.draw = function() {
         case 'ringchart':
             this.drawRingChart();
             break;
+        case 'barchart':
+            this.drawBarChart();
+            break;
         default:
             this.drawPieChart();
             break;
     }
+};
+
+fchart.prototype.drawBarChart = function(){
+    var ctx = this.ctx;
 };
 
 fchart.prototype.drawRingChart = function() {
@@ -131,8 +126,8 @@ fchart.prototype.drawRingChart = function() {
             continue;
         }
         endDeg = startDeg + deg;
-        startRadius = this.getRadius(startDeg);
-        endRadius = this.getRadius(endDeg);
+        startRadius = utils.getRadius(startDeg);
+        endRadius = utils.getRadius(endDeg);
         //store info
         this.data.info[key].deg = deg;
         this.data.info[key].startDeg = startDeg;
@@ -149,14 +144,14 @@ fchart.prototype.drawRingChart = function() {
         ctx.closePath();
 
 
-        // drawing white border
+        /*// drawing white border
         ctx.beginPath();
         ctx.moveTo(this.cx, this.cy);
         ctx.lineTo(endPos.x, endPos.y);
         ctx.lineWidth = 1;
         ctx.strokeStyle = '#ffffff';
         ctx.stroke();
-        ctx.closePath();
+        ctx.closePath();*/
 
         // next sector data
         startDeg = endDeg;
@@ -188,8 +183,8 @@ fchart.prototype.drawPieChart = function(){
             continue;
         }
         endDeg = startDeg + deg;
-        startRadius = this.getRadius(startDeg);
-        endRadius = this.getRadius(endDeg);
+        startRadius = utils.getRadius(startDeg);
+        endRadius = utils.getRadius(endDeg);
         //store info
         this.data.info[key].deg = deg;
         this.data.info[key].startDeg = startDeg;
@@ -208,14 +203,14 @@ fchart.prototype.drawPieChart = function(){
         ctx.closePath();
 
 
-        // drawing white border
-        ctx.beginPath();
-        ctx.moveTo(this.cx, this.cy);
-        ctx.lineTo(endPos.x, endPos.y);
-        ctx.lineWidth = 1;
-        ctx.strokeStyle = '#ffffff';
-        ctx.stroke();
-        ctx.closePath();
+        // // drawing white border
+        // ctx.beginPath();
+        // ctx.moveTo(this.cx, this.cy);
+        // ctx.lineTo(endPos.x, endPos.y);
+        // ctx.lineWidth = 1;
+        // ctx.strokeStyle = '#ffffff';
+        // ctx.stroke();
+        // ctx.closePath();
 
         // next sector data
         startDeg = endDeg;
@@ -225,194 +220,17 @@ fchart.prototype.drawPieChart = function(){
     }
 };
 
+// draw label and data
 fchart.prototype.drawLabel = function() {
-    var ctx = this.ctx;
-    var posStore = {};
-
-    // this.data['male'].figure = 250;
-    // this.data['female'].figure = 190;
-    // this.data['unknown'].figure = 5;
-    // this.dataPercentage['male'] = 0.65;
-    // this.dataPercentage['female'] = 0.30;
-    // this.dataPercentage['unknown'] = 0.05;
-
-    var totalBits = 0;
-    var multiplier = 0;
-    var i = 0;
-    // draw absolute number
-    for (key in this.data) {
-        var bts = this.getBits(this.data[key].figure);
-        switch (i) {
-            case 0:
-                multiplier += (bts > 2) ? 3 : 2; 
-                multiplier += (bts === 4) ? 1 : 2; 
-            break;
-            case 1:
-                 multiplier += 4;  
-            break;
-            case 2: 
-                multiplier += (bts > 2) ? 3 : 2; 
-                multiplier += (bts === 4) ? 1 : 2; 
-            break;
-        }
-        totalBits = totalBits + bts; 
-        i++;
-    }
-
-    // console.log(multiplier);
-    var numFontSize = 72;
-    ctx.font = numFontSize + 'px sans-serif';
-    var numWidth = ctx.measureText(1).width;
     
-    var percentFontSize = 28;
-    ctx.font = percentFontSize + 'px sans-serif';
-    var percentWidth = ctx.measureText(1).width;
+};
 
+fchart.prototype.drawText = function() {
     
-    var prePosX = 0;
-    var prePosY = 0;
-    var index = 0;
-
-    for (key in this.data) {
-        if (index === 0) {
-            var bits0 = this.getBits(this.data[key].figure);
-            
-            // console.log(numWidth);
-            var numTotalWidth = numWidth * multiplier;
-            posStore[key] = {};
-            posStore[key].xPos = this.cx - numTotalWidth / 2;
-            posStore[key].yPos = this.cy + 300;
-            posStore[key].bits = bits0;
-        }
-        else if (index === 1) {
-            var bits1 = this.getBits(this.data[key].figure);
-            var gap1 = (bits1 > 3) ? 1 : 2;
-            posStore[key] = {};
-            posStore[key].xPos = prePosX + (bits0 + gap1) * numWidth + (4 - bits1) * numWidth / 2;
-            posStore[key].yPos = prePosY;
-            posStore[key].bits = bits1;
-        }
-        else if (index === 2) {
-            var bits2 = this.getBits(this.data[key].figure);
-            var gap2 = (bits2 > 3) ? 1 : 2;
-            posStore[key] = {};
-            posStore[key].xPos = prePosX + (4 + gap2) * numWidth;
-            posStore[key].yPos = prePosY;
-            posStore[key].bits = bits2;
-        }
-
-        ctx.font = numFontSize + "px -apple-system-font, \"Helvetica Neue\", Helvetica, STHeiTi, sans-serif";
-        ctx.fillStyle = '#666666';
-        ctx.fillText(this.data[key].figure, posStore[key].xPos, posStore[key].yPos);
-        var percentNum = Math.ceil(this.dataPercentage[key] * 100);
-        var percentBits = this.getBits(percentNum) + 1;
-        ctx.font = percentFontSize + "px -apple-system-font, \"Helvetica Neue\", Helvetica, STHeiTi, sans-serif";
-        ctx.fillStyle = '#999999';
-        ctx.fillText(percentNum + '%', posStore[key].xPos + (posStore[key].bits * numWidth - percentBits * percentWidth) / 2, 
-                     posStore[key].yPos + 65);
-
-        var icon = this.data[key].icon;
-        ctx.drawImage(this.img, icon.x, icon.y, icon.w, icon.h, posStore[key].xPos + (posStore[key].bits * numWidth - icon.sw) / 2, 
-                      posStore[key].yPos + 100, icon.sw, icon.sh)
-
-        prePosX = posStore[key].xPos;
-        prePosY = posStore[key].yPos;
-        index++
-    }
 };
 
-fchart.prototype.drawNumber = function(key, degHalf, endPos, xDelta, yDelta, iconLen) {
-    // var ctx = this.ctx;
-    // var percentage = Math.round(this.dataPercentage[key] * 100);
-    // var bits = this.getBits(this.data[key].figure);
-    // var fontSize = (this.canvas.width > 320) ? 44 : 32;
-    // var marginHorizontal = 10;
-    // var marginVertical = 20;
-    // var numMargin = 0;
-    // var percentMargin = 0;
-    // ctx.font = fontSize + 'px sans-serif';
-    // var numWidth = ctx.measureText(this.data[key].figure).width;
-    // ctx.font = "30px sans-serif";
-    // var percentWidth = ctx.measureText(percentage + '%').width;
-
-    // if (degHalf <= 180){
-    //     iconLen *= 1;
-    //     marginHorizontal *= 1;
-    //     numMargin += (xDelta + iconLen + marginHorizontal);
-    //     percentMargin += (xDelta + iconLen + marginHorizontal);
-    // }
-    // else {
-    //     iconLen *= -1;
-    //     marginHorizontal *= -1;
-    //     numWidth *= -1;
-    //     numMargin += (xDelta + numWidth + marginHorizontal * 1.5);
-    //     percentWidth *= -1;
-    //     percentMargin += (xDelta +  percentWidth + marginHorizontal * 1.5);
-    // }
-    // var numX = endPos.x + numMargin;
-    // var numY = endPos.y + yDelta +  marginVertical;
-    // var percentX = endPos.x + percentMargin;
-    // var percentY =endPos.y + yDelta + fontSize +  marginVertical;
-
-    // var avoidCollisionDeg = degHalf;
-    // var result = this.checkCollision(key, degHalf, numWidth, fontSize, percentWidth, 30, numX, numY, iconLen, marginHorizontal);
-    // if (result) {
-    //     avoidCollisionDeg += this.changedDeg;
-    //     avoidCollisionDeg = (avoidCollisionDeg - 360 >= 0) ? (avoidCollisionDeg - 360): avoidCollisionDeg;
-    //     this.changedDeg += 10;
-    //     this.drawLabel(key, 0, avoidCollisionDeg);
-    //    return false;
-    // }
-    // this.changedDeg = 10;
-
-    // ctx.font = fontSize + "px -apple-system-font, \"Helvetica Neue\", Helvetica, STHeiTi, sans-serif";
-    // ctx.fillStyle = this.data[key].color;
-    // ctx.fillText(this.data[key].figure, numX, numY);
-
-    // ctx.font = "30px -apple-system-font, \"Helvetica Neue\", Helvetica, STHeiTi, sans-serif";
-    // ctx.fillText(percentage + '%', percentX, percentY);
-    // return true;
-};
-
-fchart.prototype.drawTotalNumber = function() {
-    var ctx = this.ctx;
-    ctx.beginPath();
-    ctx.arc(this.cx, this.cy, this.r - 40, 0, Math.PI*2, true);
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fill();
-    ctx.closePath();
-
-    var bits = this.getBits(this.total);
-    var fontSize = (bits <= 3) ? 120 : 100;
-    ctx.fillStyle = '#666666';
-    ctx.font = fontSize + "px -apple-system-font, \"Helvetica Neue\", Helvetica, STHeiTi, sans-serif";
-    var textWidth = ctx.measureText(this.total).width;
-    ctx.fillText(this.total, this.cx - textWidth / 2 + 4, this.cy);
-
-    var tag = '报名人数';
-    ctx.fillStyle = '#666666';
-    ctx.font = "48px -apple-system-font, \"Helvetica Neue\", Helvetica, STHeiTi, sans-serif";
-    textWidth = ctx.measureText(tag).width;
-    ctx.fillText(tag, this.cx - textWidth / 2 + 4, this.cy + 80);
-
-};
-
-fchart.prototype.getBits = function(num) {
-    var bits = 0;
-    while (true) {
-        num /= 10;
-        bits++;
-        if (num < 1) {
-            break;
-        }
-
-    }
-    return bits;
-};
-
-//convert degree to radius
-fchart.prototype.getRadius = function(deg) {
-    return deg / 180 * Math.PI;
+fchart.prototype.drawNum = function() {
+    
 };
 
 // get end line of sector position
@@ -423,26 +241,26 @@ fchart.prototype.getPos = function(currentDeg, lineToPos, r) {
 
     if (currentDeg <= 90) {
         deg = 90 - currentDeg;
-        radius = this.getRadius(deg);
+        radius = utils.getRadius(deg);
         lineToPos.x = this.cx + Math.cos(radius) * r;
         lineToPos.y = this.cy - Math.sin(radius) * r;
     }
     else if (currentDeg <= 180) {
         deg = currentDeg - 90;
-        radius = this.getRadius(deg);
+        radius = utils.getRadius(deg);
         lineToPos.x = this.cx + Math.cos(radius) * r;
         lineToPos.y = this.cy + Math.sin(radius) * r;
     }
     else if (currentDeg <= 270) {
         deg = 270 - currentDeg;
-        radius = this.getRadius(deg);
+        radius = utils.getRadius(deg);
         lineToPos.x = this.cx - Math.cos(radius) * r;
         lineToPos.y = this.cy + Math.sin(radius) * r;
     }
     else if (currentDeg <= 360) {
         deg = currentDeg - 270;
-        radius = this.getRadius(deg);
+        radius = utils.getRadius(deg);
         lineToPos.x = this.cx - Math.cos(radius) * r;
         lineToPos.y = this.cy - Math.sin(radius) * r;
     }
-}
+};
